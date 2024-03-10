ca9a19eddf6d8a1166468ecc2b01c317efcf4c33
Sam Barrett <sam@barretthome.uk>
2024-03-10 14:28:21 +0000

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/barrett370/gorepos
+
+go 1.22.0
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
 	"log"
+	"log/slog"
 	"os"
 	"path"
 )
@@ -16,41 +16,36 @@ type Package struct {
 	Repo     string
 	Path     string
 	Packages []string
-	Url      string
+	URL      string
 }
 
 func writePage(w io.Writer, path string, pkg Package) error {
-	t, err := template.ParseFiles("layouts/gorepos.html")
+	t, err := template.ParseFiles("template.html")
 	if err != nil {
 		return err
 	}
 	pkg.Path = path
-	var page bytes.Buffer
-	err = t.Execute(&page, pkg)
+	err = t.Execute(w, pkg)
 	if err != nil {
 		return err
 	}
-
-	_, err = io.Copy(w, &page)
-	return err
+	return nil
 }
 
 func loadConfig(namespace string) (ret []Package, err error) {
-	content, err := ioutil.ReadFile(fmt.Sprintf("%s.json", namespace))
+	var f *os.File
+	f, err = os.Open(fmt.Sprintf("%s.json", namespace))
 	if err != nil {
 		return
 	}
-	err = json.Unmarshal(content, &ret)
+	err = json.NewDecoder(f).Decode(&ret)
 	return
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("main.go namespace fs-root-dir\n")
-	}
-
-	namespace := os.Args[1]
-	root := os.Args[2]
+	flag.Parse()
+	namespace := flag.Arg(0)
+	root := flag.Arg(1)
 
 	packages, err := loadConfig(namespace)
 	if err != nil {
@@ -67,17 +62,20 @@ func main() {
 			root := path.Join(root, pkg.Path, subpackage)
 
 			if err := os.MkdirAll(root, 0755); err != nil {
-				fmt.Errorf("%s\n", err)
+				slog.Error("error", "err", err)
 				return
 			}
 
 			fd, err := os.Create(path.Join(root, "index.html"))
 			if err != nil {
-				fmt.Errorf("%s\n", err)
+				slog.Error("error", "err", err)
 				return
 			}
 			defer fd.Close()
-			writePage(fd, pkgpath, pkg)
+			err = writePage(fd, pkgpath, pkg)
+			if err != nil {
+				slog.Error("error", "err", err)
+			}
 			fd.Close()
 		}
 	}
